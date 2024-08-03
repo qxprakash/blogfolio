46a9b132a1de3ccd9480a71f1722f00540f6c2a1
Prakash Kumar <qxprakash@gmail.com>
2024-08-03 15:37:44 +0530

    added assets and logo svg as a component

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import LogoIcon from '@/assets/logo.svg'
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="py-4">
+      <div className="container">
+        <div className="flex justify-between">
+          <div>
+            <LogoIcon className="h-8 w-8"/>
+          </div>
+          <div>
+            <button>
+                Join Waitlist
+            </button>
+            <span>
+                menu
+            </span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
