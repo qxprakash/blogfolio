b5874b39dc314c71332a9cfd3cc1e5ecf6638a2a
Prakash Kumar <qxprakash@gmail.com>
2024-08-12 16:04:15 +0530

    icons update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
       <Header />
       <Hero />
       <FooterWrapper />
-
     </>
   );
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { FaHome, FaSearch, FaUser } from 'react-icons/fa';
+import { FaHome, FaFolderOpen} from 'react-icons/fa';
+import { LuFeather } from "react-icons/lu";
+import { RiHomeHeartLine, RiGitRepositoryFill } from "react-icons/ri";
+
 
 const Footer = () => {
   return (
     <footer className='fixed left-0 bottom-0 w-full bg-black bg-opacity-30 backdrop-blur-lg border-t-2 border-white shadow-lg'>
       <div className='flex justify-around items-center h-16'>
         <a href="#" className='text-white text-2xl'>
-          <FaHome />
+          <RiHomeHeartLine />
         </a>
         <a href="#" className='text-white text-2xl'>
-          <FaSearch />
+          <RiGitRepositoryFill />
         </a>
         <a href="#" className='text-white text-2xl'>
-          <FaUser />
+          <LuFeather />
         </a>
       </div>
     </footer>
