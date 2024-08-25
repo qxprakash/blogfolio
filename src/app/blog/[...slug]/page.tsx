11b1a03aa59b4b869dbe7dc887fa148a76a93005
Prakash Kumar <qxprakash@gmail.com>
2024-08-25 17:20:18 +0530

    mdx code block styles and ts error fix

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -1,6 +1,7 @@
 import { posts } from "#site/content";
 import { MDXContent } from "@/components/mdx-components";
 import { notFound } from "next/navigation";
+import "@/styles/mdx.css";
 
 interface PostPageProps {
   params: {
@@ -23,7 +24,7 @@ export async function generateStaticParams(): Promise<
 export default async function PostPage({ params }: PostPageProps) {
   const post = await getPostFromParams(params);
   if (!post || !post.published) {
-    notFound;
+    notFound();
   }
   return (
     <article className="container py-6 prose dark:prose-invert max-w-3xl mx-auto">
@@ -32,7 +33,7 @@ export default async function PostPage({ params }: PostPageProps) {
         <p className="text-xl mt-0 text-muted-foreground">{post.description}</p>
       ) : null}
       <hr className="my-4" />
-      <MDXContent code={post.body} />
+      <MDXContent code={post?.body} />
     </article>
   );
 }
